fbeaa224d5daf9fe6e50fc6dd323911465e06f0d
wujianwu <824838259@qq.com>
2023-08-14 23:46:21 +0800

    【修复】兼容Android11读文件权限

--- framework/src/main/java/nostalgia/framework/ui/SplashActivity.java
+++ framework/src/main/java/nostalgia/framework/ui/SplashActivity.java
@@ -5,8 +5,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.blankj.utilcode.constant.PermissionConstants;
 import com.blankj.utilcode.util.PermissionUtils;
@@ -22,20 +25,45 @@ import nostalgia.framework.R;
 
 public class SplashActivity extends Activity {
 
+    private static final int GET_ALL_FILE_ACCESS = 673;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                startWithPermission();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (Environment.isExternalStorageManager()) {
+                Intent intent = new Intent();
+                intent.setAction(getString(R.string.action_gallery_page));
+                startActivity(intent);
+                finish();
+            } else {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivityForResult(intent, GET_ALL_FILE_ACCESS);
             }
-        }, 800L);
+        }else{
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    startWithPermission();
+                }
+            }, 800L);
+        }
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_ALL_FILE_ACCESS && resultCode == RESULT_OK) {
+            Intent intent = new Intent();
+            intent.setAction(getString(R.string.action_gallery_page));
+            startActivity(intent);
+            finish();
+        }
+    }
+
     private void startWithPermission() {
         PermissionUtils.permission(PermissionConstants.STORAGE)
                 .callback(new PermissionUtils.SimpleCallback() {
