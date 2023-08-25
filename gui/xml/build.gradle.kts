d91654075f2617a07ecdc1b81023d9e13837867d
吴剑舞 <46742928+woohyman@users.noreply.github.com>
2023-08-25 14:23:43 +0800

    【版本】版本改成0.0.4

--- gui/xml/build.gradle.kts
+++ gui/xml/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.woohyman.emulator"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
